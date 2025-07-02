359b6c3297ffcea639b1b0313d3d1fa2df1b3e3b
Satyamray21 <satyamray43@gmail.com>
2025-07-02 17:13:41 +0530

    added come changes.

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
-import asyncHandler from "../utils/asyncHandler.js";
+import {asyncHandler} from "../utils/asyncHandler.js";
 
 export const isAuthenticated = asyncHandler(async (req, res, next) => {
   const token = req.cookies?.accessToken;
